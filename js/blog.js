a289969dab97b89d55ee71bfc7ec7d207d96a409
Edghani Orihuela <rainuxidk@gmail.com>
2022-11-19 12:02:38 -0500

    Update 19-11-22
    Arreglo de algunas cosas

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,18 @@
-$('.menu-bar').on('click', function() {
-    $('nav ul').toggleClass('reveal');
+$(document).ready(() => {
+  $('#mensaje').hide()
+  $('#mensaje2').hide()
+
+  $('#leerMas').click(function() {
+      $('#intro').hide()
+      $('#leerMas').hide()
+
+      $('#mensaje').show('slow')
+  })
+
+  $('#leerMas2').click(function() {
+    $('#intro2').hide()
+    $('#leerMas2').hide()
+
+    $('#mensaje2').show('slow')
+})
 })
